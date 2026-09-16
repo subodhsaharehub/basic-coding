c53d4562aff93f8735a81b132d9036e44f850020
subodhsaharehub <subodhsaharexyz@gmail.com>
2026-09-16 19:09:08 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,14 +1,14 @@
-// #include <iostream>
-// using namespace std;
-// int main (){
-//     int i,n,fact = 1;
-//     cout<<"enter the no. = ";
-//     cin>>n;
+#include <iostream>
+ using namespace std;
+int main (){
+     int i,n,fact = 1;
+     cout<<"enter the no. = ";
+     cin>>n;
 
-//     for( i = 1; i<=n; i++ ){
+    for( i = 1; i<=n; i++ ){
 
-//         fact = fact*i;
-//     }
-//         cout<<fact;
-//     return 0;
-// }
+         fact = fact*i;
+     }
+         cout<<fact;
+     return 0;
+ }
