9cc8d64513e259a51edacbfc4633a9d29469e46d
subodhsaharehub <subodhsaharexyz@gmail.com>
2026-09-16 07:33:01 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float r;
+//     cout<<"enter the radius = ";
+//     cin>>r;
+
+//     float area = 3.14*r*r;
+    
+//     cout<<area;
+//     return 0;
+// }
