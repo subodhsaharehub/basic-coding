6ad303ed7874b45bc445f0168bf8f216f16df473
subodhsaharehub <subodhsaharexyz@gmail.com>
2026-09-16 07:32:14 +0530

    Update addingtwonumbers.cpp

--- addingtwonumbers.cpp
+++ addingtwonumbers.cpp
@@ -1,7 +1,8 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int a = 10;
-//     int b = 20;
-//     cout<<"a+b = "<<a+b;
-// }
+ #include <iostream>
+ using namespace std;
+ int main()
+{
+     int a = 10;
+     int b = 20;
+     cout<<"a+b = "<<a+b;
+ }
