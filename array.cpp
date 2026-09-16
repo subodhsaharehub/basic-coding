2a8fe5469fa9781962d82b521b8891622128134f
subodhsaharehub <subodhsaharexyz@gmail.com>
2026-09-16 07:44:17 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int i, arr [5];
+    cout<<"enter the values";
+
+    for( i = 0; i<5; i++){
+        cin>>arr[i];
+    }
+    for(i = 0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
