eb6206b1e3132484cf25e1db753eb7957d24d9b7
subodhsaharehub <subodhsaharexyz@gmail.com>
2026-09-16 07:43:39 +0530

    Update circumferenceofcircle.cpp

--- circumferenceofcircle.cpp
+++ circumferenceofcircle.cpp
@@ -1,13 +1,13 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
+ #include <iostream>
+ using namespace std;
+ int main(){
 
-//     float r;
-//     cout<<"enter the radius = ";
-//     cin>>r;
+     float r;
+     cout<<"enter the radius = ";
+    cin>>r;
 
-//     float circum = 2*3.14*r;
+     float circum = 2*3.14*r;
 
-//     cout<<circum;
-//     return 0;
-// }
+    cout<<circum;
+     return 0;
+ }
