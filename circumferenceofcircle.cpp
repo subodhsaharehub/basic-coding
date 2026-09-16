967a8a9d43891236f0c89160d052833979662e47
subodhsaharehub <subodhsaharexyz@gmail.com>
2026-09-16 07:36:41 +0530

    Add files via upload

---
+++ circumferenceofcircle.cpp
@@ -0,0 +1,13 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     float r;
+//     cout<<"enter the radius = ";
+//     cin>>r;
+
+//     float circum = 2*3.14*r;
+
+//     cout<<circum;
+//     return 0;
+// }
